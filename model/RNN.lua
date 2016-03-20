e9e92f02c29e527d7c7def47e1ddd21231e4c97c
squidszyd <squidroidnxs@gmail.com>
2016-03-20 15:19:07 +0800

    Add comments on usage of nngraph

--- model/RNN.lua
+++ model/RNN.lua
@@ -10,6 +10,18 @@ function RNN.rnn(input_size, rnn_size, n, dropout)
 
   end
 
+  --[[
+  NOTE-zyd:
+    What does the two () in "nn.Identity()()" mean?
+    Ans:
+    nngraph overloads the call operator(i.e. the () operator, which is used for function calls)
+    on all the nn.Module objects.When the call operator is invoked, it returns a node wrapping
+    the nn.Module. The call operator takes the parents of the node as arguments, which specify 
+    which modules will feed into this one during a forward pass.
+    Therefore, nn.Identity() is a nn.Module while nn.Identity()() is a graph node.
+    For detailed info, go to https://github.com/torch/nngraph/
+  ]]--
+
   local x, input_size_L
   local outputs = {}
   for L = 1,n do
@@ -20,7 +32,8 @@ function RNN.rnn(input_size, rnn_size, n, dropout)
       input_size_L = input_size
     else 
       x = outputs[(L-1)] 
-      if dropout > 0 then x = nn.Dropout(dropout)(x) end -- apply dropout, if any
+      if dropout > 0 then x =  The call
+operator takes the parents of the node as arguments, nn.Dropout(dropout)(x) end -- apply dropout, if any
       input_size_L = rnn_size
     end
 
