20d7ce8366fd2593af2b06172f762f3dab949659
squidszyd <squidroidnxs@gmail.com>
2016-03-21 21:10:52 +0800

    Almost figure out how to implement a LSTM

--- model/LSTM.lua
+++ model/LSTM.lua
@@ -2,21 +2,26 @@
 local LSTM = {}
 function LSTM.lstm(input_size, rnn_size, n, dropout)
   dropout = dropout or 0 
-
+  --[[
+  NOTE-zyd: 
+        input_size: inputs vector size;
+        rnn_size: num of hidden units per layer, or time depth
+        n: depth of the network
+  ]]--
   -- there will be 2*n+1 inputs
   local inputs = {}
-  table.insert(inputs, nn.Identity()()) -- x
+  table.insert(inputs, nn.Identity()()) --NOTE-zyd: x, network inputs
   for L = 1,n do
-    table.insert(inputs, nn.Identity()()) -- prev_c[L]
-    table.insert(inputs, nn.Identity()()) -- prev_h[L]
+    table.insert(inputs, nn.Identity()()) --NOTE-zyd: prev_c[L], c at time t-1
+    table.insert(inputs, nn.Identity()()) --NOTE-zyd: prev_h[L], h at time t-1
   end
 
   local x, input_size_L
   local outputs = {}
   for L = 1,n do
     -- c,h from previos timesteps
-    local prev_h = inputs[L*2+1]
-    local prev_c = inputs[L*2]
+    local prev_h = inputs[L*2+1] --NOTE-zyd: h at time t-1 on layer L.
+    local prev_c = inputs[L*2]   --NOTE-zyd: c at time t-1 on layer L.
     -- the input to this layer
     if L == 1 then 
       x = OneHot(input_size)(inputs[1])
@@ -30,7 +35,10 @@ function LSTM.lstm(input_size, rnn_size, n, dropout)
     local i2h = nn.Linear(input_size_L, 4 * rnn_size)(x):annotate{name='i2h_'..L}
     local h2h = nn.Linear(rnn_size, 4 * rnn_size)(prev_h):annotate{name='h2h_'..L}
     local all_input_sums = nn.CAddTable()({i2h, h2h})
-
+    --[[
+    NOTE-zyd:
+          Here, 'all_input_sums' outputs a vector created by a linear transform of inputs and previous hidden state. These are raw values which will be used to compute the gate activations and the cell inputs. The vector is divided into 4 parts, each of size 'rnn_size'.They are IN-GATE, FORGET-GATE, OUT-GATE and CELL-INPUT.
+    ]]--
     local reshaped = nn.Reshape(4, rnn_size)(all_input_sums)
     local n1, n2, n3, n4 = nn.SplitTable(2)(reshaped):split(4)
     -- decode the gates
